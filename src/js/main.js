d95dc1544bfc58b7acbd3f05ca8c93c501243e77
“CarlotaBlanco” <lotsilots@gmail.com>
2022-07-06 12:36:22 +0200

    add favourites functionality

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,10 @@ const inputSearch = document.querySelector('.js-input');
 const buttonSearch = document.querySelector('.js-button');
 const resultsList = document.querySelector('.js-listElements');
 const resetButton = document.querySelector('.js-reset');
+const favouritesList = document.querySelector('.js-favourites');
 
 let heroes = [];
+let favourites = [];
 
 function validateInput() {
   if (inputSearch.value === '') {
@@ -23,7 +25,6 @@ function getApiData() {
     .then((data) => {
       heroes = data.results;
       renderResults(heroes);
-      console.log(heroes);
     });
 }
 function renderResults(heroes) {
@@ -32,19 +33,63 @@ function renderResults(heroes) {
     html = `nooooo`;
   } else {
     for (const heroe of heroes) {
-      html += `<li class="listresults__item">`;
+      html += `<li class="js-listresults listresults__item" id="${heroe.id}">`;
       html += `<img class="listresults__item--img" src="${heroe.image.url}" alt="Superheroe profile pic"/>`;
       html += `<p  class="listresults__item--name" >${heroe.name}</p>`;
-
       html += `</li>`;
     }
   }
   resultsList.innerHTML = html;
+  cardListener();
+}
+function cardListener() {
+  const cardLi = document.querySelectorAll('.js-listresults');
+  for (const card of cardLi) {
+    card.addEventListener('click', addFavourites);
+  }
+}
+
+function addFavourites(event) {
+  const idCard = event.currentTarget.id;
+  const favoriteFound = heroes.find((fav) => {
+    return fav.id === idCard;
+  });
+  const favoriteFoundIndex = favourites.findIndex((fav) => {
+    return fav.id === idCard;
+  });
+  if (favoriteFoundIndex === -1) {
+    favourites.push(favoriteFound);
+  } else {
+    favourites.splice(favoriteFoundIndex, 1);
+  }
+  console.log(heroes);
+  console.log(favourites);
+  renderFavourites();
+}
+
+function renderFavourites() {
+  let html = '';
+  for (const favourite of favourites) {
+    html += `<li class="js-favourite_results listresults__item" id="${favourite.id}">`;
+    html += `<img class="listresults__item--img" src="${favourite.image.url}" alt="Superheroe profile pic"/>`;
+    html += `<p  class="listresults__item--name" >${favourite.name}</p>`;
+    html += `</li>`;
+  }
+  favouritesList.innerHTML = html;
+  favouritesListener();
+}
+
+function favouritesListener() {
+  const favouriteLi = document.querySelectorAll('.js-favourite_results');
+  for (const favourite of favouriteLi) {
+    favourite.addEventListener('click', addFavourites);
+  }
 }
 
 function resetResults() {
   heroes = [];
   resultsList.innerHTML = '';
+  favouritesList.innerHTML = '';
   inputSearch.value === '';
 }
 
